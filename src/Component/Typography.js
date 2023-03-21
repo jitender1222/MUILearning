98ca2c43abeea781bec8bce38bee90f98e7423e1
jitender singh <jeetusinghkanyal@gmail.com>
2023-03-21 11:47:02 +0530

    added typography

---
+++ src/Component/Typography.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Typo = () => {
+  return (
+    <div>
+      <Typography variant="h3" mt={2}>
+        Text written inside the Typography
+      </Typography>
+    </div>
+  );
+};
+
+export default Typo;
