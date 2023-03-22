19396e02f23e6103c4c5cedde6ec593ca5717b29
jitender singh <jeetusinghkanyal@gmail.com>
2023-03-22 22:33:47 +0530

    changes in home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import hero from "../images/Foods.jpg";
+import "../Component/HeaderStyle.css";
 const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <div
+        className="home"
+        style={{
+          backgroundImage: `url(${hero})`,
+        }}
+      >
+        <div className="hero">
+          <h1>Food Website</h1>
+          <p>Best Food in India</p>
+          <Link to={"/menu"}>
+            <button>Order Now</button>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
